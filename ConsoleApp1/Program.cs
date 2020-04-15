bfc3ba72906d5d8a9d008d8bdcf45e10d495823f
jonatan.maxi.ponce@gmail.com <jonatan.maxi.ponce@gmail.com>
2020-04-15 00:35:14 -0300

    Repaso de Diccionario e implementacion del metodo Add(agregar)

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> personas = new Dictionary<int, string>();
+            personas.Add(1, "julia");
+            personas.Add(2, "martin");
+            personas.Add(3, "jonah");
+
+            Console.WriteLine(personas[1]);
+        }
+    }
+}
