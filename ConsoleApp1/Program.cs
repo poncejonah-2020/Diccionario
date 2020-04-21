b42c7ce20cf72f632843559191e362136f730cc0
jonatan.maxi.ponce@gmail.com <jonatan.maxi.ponce@gmail.com>
2020-04-20 23:11:49 -0300

    diccionario, se agrega el metodo remove en 2 opciones diferentes y la forma de recorrer un diccionario

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,8 +11,27 @@ namespace ConsoleApp1
             personas.Add(1, "julia");
             personas.Add(2, "martin");
             personas.Add(3, "jonah");
+            personas.Add(4, "yesica");
+            personas.Add(5, "sebas");
 
             Console.WriteLine(personas[1]);
+            Console.ReadKey();
+
+            personas.Remove(2);
+
+            if(personas.Remove(3))
+            {
+                Console.WriteLine("el elemento fue eliminado");
+            }
+            else
+            {
+                Console.WriteLine("el elemento no fue encontrado");
+            }
+
+            foreach(KeyValuePair<int, string> elemento in personas)
+            {
+                Console.WriteLine("clave " + elemento.Key + ":" + elemento.Value);
+            }
         }
     }
 }
